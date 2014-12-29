853f854b76d80294b6550f1afb18af5ce005768a
Jérémy Jousse <jeremy.jousse@coevo.fr>
2014-12-29 11:24:09 +0100

    Add deploy gem

--- config.rb
+++ config.rb
@@ -47,6 +47,15 @@
 #   end
 # end
 
+activate :deploy do |deploy|
+  deploy.method = :git
+  # Optional Settings
+  deploy.remote   = 'origin' # remote name or git url, default: origin
+  deploy.branch   = 'gh-pages' # default: gh-pages
+  # deploy.strategy = :submodule      # commit strategy: can be :force_push or :submodule, default: :force_push
+  # deploy.commit_message = 'custom-message'      # commit message (can be empty), default: Automated commit at `timestamp` by middleman-deploy `version`
+end
+
 set :css_dir, 'stylesheets'
 
 set :js_dir, 'javascripts'
